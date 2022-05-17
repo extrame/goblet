0f64e097df947d00e5edc4459118265030b00321
Liu Ming <extraf@live.cn>
2022-05-17 09:45:23 +0800

    add params to control where we call Error for error or not

--- plugin/json.go
+++ plugin/json.go
@@ -3,6 +3,9 @@ package plugin
 import "github.com/extrame/goblet"
 
 type _JSONPlugin struct {
+	//some error is object and use Error() to response string, this attribute can use to control
+	//whether we response Error as String by call Error() or just resonse Error Obbject
+	ResponseOriginalError bool
 }
 
 var JSON = new(_JSONPlugin)
@@ -12,7 +15,11 @@ func (p *_JSONPlugin) RespendOk(ctx *goblet.Context) {
 }
 
 func (p *_JSONPlugin) RespondError(ctx *goblet.Context, err error, context ...string) {
-	ctx.AddRespond("Error", err.Error())
+	if p.ResponseOriginalError {
+		ctx.AddRespond("Error", err)
+	} else {
+		ctx.AddRespond("Error", err.Error())
+	}
 	if len(context) > 0 {
 		ctx.AddRespond("Context", context)
 	}
